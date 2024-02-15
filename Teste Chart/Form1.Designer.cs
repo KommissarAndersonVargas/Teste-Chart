1e33185e25677ad6299dee02ef16bcaf8dfe6fa8
Anderson Vargas <andersomrobertomachadodevargas@gmail.com>
2024-02-15 19:38:43 -0300

    Adicionar arquivos de projeto.

---
+++ Teste Chart/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Teste_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(315, 95);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load ProgressBar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(333, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(465, 237);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Add +";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(652, 253);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Add -";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Teste Chart/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Teste_Chart
+{
+    public partial class Form1 : Form
+    {
+        int inc = 0;
+        int inc2 = 1;
+
+        public Form1()
+        {
+         
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = 100;
+            progressBar1.ForeColor = Color.Yellow;
+            progressBar1.Step = 1;
+            int valor = 10;
+
+            progressBar1.Value = valor + progressBar1.Value;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GerarColunas();
+                Title title = new Title();
+                title.Font = new Font("Arial", 14, FontStyle.Bold);
+                title.ForeColor = Color.Blue;
+                title.Text = "Memoria x Tempo";
+                chart1.Titles.Add(title);
+            }
+            catch(Exception EX)
+            {
+                MessageBox.Show("Erro: " + EX);
+            }
+
+        }
+        private void GerarColunas()
+        {
+            /*
+            Title title  = new Title();
+            title.Font = new Font("Arial", 14, FontStyle.Bold);
+            title.ForeColor = Color.BlueViolet;
+            title.Text = "Vendas";
+            chart1.Titles.Add(title);
+            
+
+            chart1.Series.Add("vendas");
+            chart1.Series["vendas"].LegendText = "Vendas";
+            chart1.Series["vendas"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+            chart1.Series["vendas"].Points.AddXY("Janeiro", 140);
+            chart1.Series["vendas"].Points.AddXY("Fevereiro",60);
+
+            */
+
+            try
+            {
+                chart1.Series[0].Points.AddXY(3, 7);
+                chart1.Series[0].Points.AddXY(4, 9);
+            }
+
+            catch (Exception EX)
+            {
+                MessageBox.Show("Erro: " + EX);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                 string dado = textBox1.Text.ToString();
+                 chart1.Series["vendas"].Points.AddXY(dado, inc = inc + 20);
+                 */
+                chart1.Series[0].Points.AddXY(inc2 = 1 + inc2, inc = inc + 3);
+
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("Erro: " + EX);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                chart1.Series[0].Points.AddXY(inc2 = 1 + inc2, inc = inc - 1);
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("Erro: " + EX);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
